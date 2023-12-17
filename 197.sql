1b1ad4f1e98c7c924bdb55e62d6ce53a05752510
Pingying Chen <30317248+Pingying-Chen@users.noreply.github.com>
2023-12-17 14:31:34 -0800

    Update 197.sql

--- 197.sql
+++ 197.sql
@@ -1,13 +1,24 @@
 # 197. Rising Temperature
 
 # Write your MySQL query statement below
-SELECT w1.id
-FROM Weather AS w1
-INNER JOIN Weather AS w2
-ON DATEDIFF(w1.RecordDate, w2.RecordDate) = 1
-WHERE w1.temperature > w2.temperature;
+SELECT
+  w1.id
+FROM
+  Weather AS w1
+INNER JOIN
+  Weather AS w2
+  ON
+    DATEDIFF(w1.RecordDate, w2.RecordDate) = 1
+WHERE
+  w1.temperature > w2.temperature;
 
 # Write your MySQL query statement below
-SELECT w1.id
-FROM Weather w1, Weather w2
-WHERE DATEDIFF(w1.recordDate, w2.recordDate) = 1 AND w1.temperature > w2.temperature;
+SELECT
+  w1.id
+FROM
+  Weather w1,
+  Weather w2
+WHERE
+  DATEDIFF(w1.recordDate, w2.recordDate) = 1
+  AND
+  w1.temperature > w2.temperature;
