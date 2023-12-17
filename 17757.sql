ccb2b6e59685b0a896114520e6c2660871c5d37b
Pingying Chen <30317248+Pingying-Chen@users.noreply.github.com>
2023-12-17 14:32:08 -0800

    Update 17757.sql

--- 17757.sql
+++ 17757.sql
@@ -1,6 +1,11 @@
 # 1757. Recyclable and Low Fat Products
 
 # Write your MySQL query statement below
-SELECT product_id
-FROM Products
-WHERE low_fats = 'Y' AND recyclable = 'Y';
+SELECT
+  product_id
+FROM
+  Products
+WHERE
+  low_fats = 'Y'
+  AND
+  recyclable = 'Y';
