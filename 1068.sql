4952c1f3ab6034e46033acd5d206fd880602ca19
Pingying Chen <30317248+Pingying-Chen@users.noreply.github.com>
2023-12-16 22:30:48 -0800

    Create 1068.sql

---
+++ 1068.sql
@@ -0,0 +1,7 @@
+# 1068. Product Sales Analysis I
+
+# Write your MySQL query statement below
+SELECT product_name, year, price
+FROM Sales AS s
+LEFT JOIN Product AS p
+ON s.product_id = p.product_id;
