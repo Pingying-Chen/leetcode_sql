9d3904f4f407f686fd8abe28a9306a64ac1974a2
Pingying Chen <30317248+Pingying-Chen@users.noreply.github.com>
2023-12-17 14:06:13 -0800

    Update 197.sql

--- 197.sql
+++ 197.sql
@@ -5,4 +5,4 @@ SELECT w1.id
 FROM Weather AS w1
 INNER JOIN Weather AS w2
 ON DATEDIFF(w1.RecordDate, w2.RecordDate) = 1
-WHERE w1.temperature > w2.temperature
+WHERE w1.temperature > w2.temperature;
