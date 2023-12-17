efa13d17985bdd0c4a81e9722974692ae5830e66
Pingying Chen <30317248+Pingying-Chen@users.noreply.github.com>
2023-12-17 14:32:28 -0800

    Update 1683.sql

--- 1683.sql
+++ 1683.sql
@@ -1,6 +1,9 @@
 # 1683. Invalid Tweets
 
 # Write your MySQL query statement below
-SELECT tweet_id
-FROM Tweets
-WHERE LENGTH(content) > 15;
+SELECT
+  tweet_id
+FROM
+  Tweets
+WHERE
+  LENGTH(content) > 15;
