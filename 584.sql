75876d09ce296d410d6280007bb11f6fefc0f2ae
Pingying Chen <30317248+Pingying-Chen@users.noreply.github.com>
2023-12-17 14:30:35 -0800

    Update 584.sql

--- 584.sql
+++ 584.sql
@@ -1,6 +1,11 @@
 # 584. Find Customer Referee
 
 # Write your MySQL query statement below
-SELECT name
-FROM Customer
-WHERE referee_id <> 2 OR referee_id IS NULL;
+SELECT
+  name
+FROM
+  Customer
+WHERE
+  referee_id <> 2
+  OR
+  referee_id IS NULL;
