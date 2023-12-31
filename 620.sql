73c8289cb46c4f2ad2ae5584b63e8c828962ca4a
Pingying Chen <30317248+Pingying-Chen@users.noreply.github.com>
2023-12-31 15:48:10 -0800

    Create 620.sql

---
+++ 620.sql
@@ -0,0 +1,13 @@
+# 620. Not Boring Movies
+
+# Write your MySQL query statement below
+
+SELECT *
+FROM Cinema
+WHERE MOD(id, 2) = 1 AND description <> 'boring'
+ORDER BY rating DESC;
+
+SELECT *
+FROM Cinema
+WHERE id%2 = 1 AND description <> 'boring'
+ORDER BY rating DESC;
