ab2621d056a8d4c1c2f7b9521d13f63d8494c368
Pingying Chen <30317248+Pingying-Chen@users.noreply.github.com>
2024-01-27 21:13:58 -0800

    Create 586.sql

---
+++ 586.sql
@@ -0,0 +1,9 @@
+# 586. Customer Placing the Largest Number of Orders
+
+# Write your MySQL query statement below
+
+SELECT customer_number
+FROM Orders
+GROUP BY customer_number
+ORDER BY COUNT(order_number) DESC
+LIMIT 1;
