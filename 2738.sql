b1785e9a5afd648cd1b04586c20363f927117ac3
Pingying Chen <30317248+Pingying-Chen@users.noreply.github.com>
2024-02-29 22:14:32 -0800

    Create 2738.sql

---
+++ 2738.sql
@@ -0,0 +1,8 @@
+# 2738. Count Occurrences in Text
+
+# Write your MySQL query statement below
+SELECT "bull" AS word, SUM(content LIKE "% bull %") AS count
+FROM Files
+UNION ALL
+SELECT "bear" AS word, SUM(content LIKE "% bear %") AS count
+FROM Files;
